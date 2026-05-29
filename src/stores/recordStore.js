afb6ba7da3ac86cc18c507c5f69c87abcc4b2e33
Daniela Campos <c21170270@culiacan.tecnm.mx>
2026-05-29 03:07:02 -0700

    Change backend URL in recordStore.js
    Updated backend URL to point to the new deployment.

--- src/stores/recordStore.js
+++ src/stores/recordStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
 
-const URL_BACKEND = import.meta.env.VITE_API_URL
+const URL_BACKEND = "https://proyectoweb-mgrx.onrender.com"
 
 export const useRecordsStore = defineStore('registros', () => {
     const pacientes = ref([]);
